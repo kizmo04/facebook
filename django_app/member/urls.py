01643009383bdf21aa96ef27187f3349333dacc9
kizmo04 <kizmo04@gmail.com>
2017-02-23 17:24:04 +0900

    Facebook - login_facebook기본구조 구현

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from member import views
 
-app_name='member'
+app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login_fbv, name='login'),
+    url(r'^login/facebook/$', views.login_facebook, name='login_facebook')
 ]
