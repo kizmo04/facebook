6f421a6339879a343e0d0f8cbf48ed61bd3c231d
kizmo04 <kizmo04@gmail.com>
2017-02-23 17:17:22 +0900

    login.html 템플릿 코드 수정, 인덱스 리다이렉트 추 가 , url수정

--- django_app/facebook/urls.py
+++ django_app/facebook/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from facebook import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^member/', include('member.urls'))
+    url(r'^member/', include('member.urls')),
+    url(r'^$', views.index, name='index'),
 ]
--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from member import views
 
+app_name='member'
 urlpatterns = [
-    url(r'^login/$', views.login_fbv, name='login')
+    url(r'^login/$', views.login_fbv, name='login'),
 ]
